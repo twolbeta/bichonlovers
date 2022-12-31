e0cee99101c6bef121f6fb18b73c6c39ac206750
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 08:10:37 -0600

    bichon1

--- functions/bichon.js
+++ functions/bichon.js
@@ -1,11 +1,11 @@
 
-exports.handler = async event => {
-  
-    return {
-      statusCode: 301,
-      headers: {
-        location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/'
-      }
+exports.handler = (event, context, callback) => {
+   const response = {
+    statusCode: 301,
+    headers: {
+      Location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/',
     }
-  
+  };
+
+  return callback(null, response);
 }
