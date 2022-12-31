590442c4cc991b127e1f32709739482943ed4a3f
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 01:28:14 -0600

    Update bichon.js

--- functions/bichon.js
+++ functions/bichon.js
@@ -1,19 +1,11 @@
 
 exports.handler = async event => {
-  if (event.headers.referer.includes('facebook')) {
+  
     return {
       statusCode: 301,
       headers: {
         location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/'
       }
     }
-  } else {
-    let pathName = location.pathname.split('/')[2].split('-')
-    return {
-      statusCode: 301,
-      headers: {
-        location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/'
-      }
-    }
-  }
+  
 }
