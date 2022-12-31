a4fdac55df75e2198f9e46b775d8e58dd774b99f
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 13:40:33 -0600

    Update bichon.js

--- functions/bichon.js
+++ functions/bichon.js
@@ -13,7 +13,7 @@ exports.handler = async event => {
       statusCode: 301,
       headers: {
         'cache-control': 'public, max-age=0, must-revalidate',
-        location: process.env.URL + '/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
+        location: decodeURIComponent(event.queryStringParameters.url)
       }
     }
   }
