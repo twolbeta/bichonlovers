c2e329e79dd6aec8eed7a4abc28a1cd1d77f8f29
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 00:44:08 -0600

    Update bichon.js

--- functions/bichon.js
+++ functions/bichon.js
@@ -1,19 +1,19 @@
+
 exports.handler = async event => {
   if (event.queryStringParameters.fbclid) {
     return {
       statusCode: 301,
       headers: {
         'cache-control': 'public, max-age=0, must-revalidate',
-        location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/'
+        location: decodeURIComponent(event.queryStringParameters.url)
       }
     }
   } else {
-    let pathName = event.path.split('/')[3].split('-')
     return {
       statusCode: 301,
       headers: {
         'cache-control': 'public, max-age=0, must-revalidate',
-        location: process.env.URL + '/' + pathName[0] + '/' + pathName[1] + '/'
+        location: process.env.URL + '/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
       }
     }
   }
