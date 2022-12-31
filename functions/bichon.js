541441cc98e3660a17948b66b509f0632774b1f4
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 08:22:28 -0600

    Update bichon.js

--- functions/bichon.js
+++ functions/bichon.js
@@ -3,7 +3,7 @@ exports.handler = (event, context, callback) => {
    const response = {
     statusCode: 301,
     headers: {
-      Location: 'https://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/',
+      Location: 'http://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/',
     }
   };
 
