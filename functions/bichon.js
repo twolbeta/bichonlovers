d892ae4f0a26cd753ea44679c686eb0d037f9153
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 10:09:53 -0600

    ww

--- functions/bichon.js
+++ functions/bichon.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   return {
     statusCode: 301,
     headers: {
-      location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
+      location: decodeURIComponent('https%3A%2F%2Fihearthwild.com%2F2020%2F07%2F06%2Fwithout-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise%2F')
     }
   }
 }
