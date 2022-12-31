6f5018fe830c8eb56931abf3bfddcb8f44ac63bf
twolbeta <121470828+twolbeta@users.noreply.github.com>
2022-12-31 09:18:14 -0600

    Update bichon.js

--- functions/bichon.js
+++ functions/bichon.js
@@ -1,31 +1,8 @@
-const fs = require('fs')
-const MarkdownIt = require('markdown-it')
-   
-exports.handler = async (event) => {
-	if (event.headers.referer && event.headers.referer.includes("facebook")) {
-    return {
-      statusCode: 301,
-      headers: {
-        location: 'http://ihearthwild.com/2020/07/06/without-hesitation-this-man-decided-to-put-his-car-up-for-sale-to-save-his-bichon-frise/'
-      }
+exports.handler = async event => {
+  return {
+    statusCode: 301,
+    headers: {
+      location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
     }
   }
-    else {
-    const { postId } = event.queryStringParameters;
-    const fileContents = fs.readFileSync(`./bichon1/bichon-${postId}.html`, 'utf8')
-    const md = new MarkdownIt();
-    const result = md.render(fileContents);
-    
-
-	return {
-        statusCode: 200,
-        headers: {
-            "Content-Type": "text/html",
-        },
-        body: result,
-    }
-	}
 }
-
-
-
