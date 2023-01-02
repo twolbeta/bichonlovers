ea5ed23daa5d6c2ac370ede5ad5e5fe0d7fb4e9c
twolbeta <121470828+twolbeta@users.noreply.github.com>
2023-01-01 23:21:04 -0600

    cambio

---
+++ Ftions/bichon-1.js
@@ -0,0 +1,20 @@
+exports.handler = async event => {
+  if (event.queryStringParameters.fbclid) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: 'https://www.google.com/'
+      }
+    }
+  } else {
+    let pathName = event.path.split('/')[3].split('-')
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + pathName[0] + '/' + pathName[1] + '/'
+      }
+    }
+  }
+}
